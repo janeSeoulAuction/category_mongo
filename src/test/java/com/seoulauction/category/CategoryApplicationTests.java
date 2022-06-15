b292f47545b35e53ce71a8bb0ee0626ccb43d163
janeSeoulAuction <102010133+janeSeoulAuction@users.noreply.github.com>
2022-06-15 11:41:01 +0900

    Update CategoryApplicationTests.java

--- src/test/java/com/seoulauction/category/CategoryApplicationTests.java
+++ src/test/java/com/seoulauction/category/CategoryApplicationTests.java
@@ -2,7 +2,7 @@ package com.seoulauction.category;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//test5
+//test5 test9
 @SpringBootTest
 class CategoryApplicationTests {
 
